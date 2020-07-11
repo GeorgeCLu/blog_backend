be13a18d505bdb133f0d441dd74c28b07656062c
George Lu <georgelu@pop-os.localdomain>
2020-07-11 16:58:40 +1200

    procfile

--- utils/config.js
+++ utils/config.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
 
-let PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 // eslint-disable-next-line max-len
 // Now we are using the port defined in environment variable PORT or port 3001 if the environment variable PORT is undefined
 // const PORT = 3001
-let MONGODB_URI = process.env.MONGODB_URI;
+let { MONGODB_URI } = process.env;
 // used to be in and used by note.js
-
+console.log("port is")
+console.log(PORT)
+console.log("mongodb uri is")
+console.log("MONGODB_URI")
 if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI;
 }
-
 module.exports = {
   MONGODB_URI,
   PORT,
