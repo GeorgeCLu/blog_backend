71d27188cccafa835af64aac8c74668ac897e086
George Lu <georgelu@pop-os.localdomain>
2020-07-11 19:16:48 +1200

    logging

--- controllers/login.js
+++ controllers/login.js
@@ -6,20 +6,26 @@ const User = require('../models/user');
 // eslint-disable-next-line consistent-return
 loginRouter.post('/', async (request, response) => {
   const { body } = request;
+  console.log("body")
+  console.log(body)
   // starts by searching for the user from the database by the username attached to the request
   // Next, it checks the password, also attached to the request.
   // Because the passwords themselves are not
   // saved to the database, but hashes calculated from the passwords,
   // the bcrypt.compare method is used to check if the password is correct:
   const user = await User.findOne({ username: body.username });
+  console.log("user")
+  console.log(user)
   const passwordCorrect = user === null
     ? false
     : await bcrypt.compare(body.password, user.passwordHash);
+    console.log("password correct")
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
       error: 'invalid username or password',
     });
   }
+  console.log("user and password correct")
   // If the password is correct,
   // a token is created with the method jwt.sign.
   // The token contains the username and the user id in a digitally signed form.
@@ -28,7 +34,11 @@ loginRouter.post('/', async (request, response) => {
     // eslint-disable-next-line no-underscore-dangle
     id: user._id,
   };
+  console.log("user for token")
+  console.log(userForToken)
   const token = jwt.sign(userForToken, process.env.SECRET);
+  console.log("token")
+  console.log(token)
   response
     .status(200)
     .send({ token, username: user.username, name: user.name });
