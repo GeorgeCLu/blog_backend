a7021d9dad8cdaae3173e833ab90d0ff5944c826
George Lu <georgelu@pop-os.localdomain>
2020-07-11 18:29:46 +1200

    uibuild

---
+++ build/precache-manifest.62a5804ddcf9088991693ab30287eb6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba5c1d34addd9dd036595a6d50e4cd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d8bbf169a85de82838e",
+    "url": "/static/js/2.06730baf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.06730baf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d711c845942d57bf05b",
+    "url": "/static/js/main.200134bd.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,21 @@
 require('dotenv').config();
 
+// .env file should be used only for local development
+// and you shouldn't commit this file to the repository.
+// The general practice on Heroku is that you configure your application by
+// setting config vars either via Heroku Dashboard or CLI command.
+// https://stackoverflow.com/questions/42633403/how-to-access-variables-in-env-file-in-node-js-app-heroku
+
+// Also when deploying project to Heroku, if you don't add .env file to your git repository,
+// then you need to give the environmental variables from Config Vars tab like here in the docs
+// https://stackoverflow.com/questions/57387809/unable-to-connect-to-mongodb-s-atlas-using-heroku-during-production
+
+// In your heroku project you need to setup an env variable ATLAS_URI
+// and enter the value for your mongodb uri.
+// To do so go to the settings tab in your heroku app,
+// then click on reveal config vars, and then enter the key and value for your mongo uri.
+// https://stackoverflow.com/questions/58082890/how-to-load-mongodb-uri-from-env-file-on-heroku
+
 const PORT = process.env.PORT || 3001;
 // eslint-disable-next-line max-len
 // Now we are using the port defined in environment variable PORT or port 3001 if the environment variable PORT is undefined
@@ -10,11 +26,6 @@ if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI;
 }
 
-console.log("port is")
-console.log(PORT)
-console.log("mongodb uri is")
-console.log(MONGODB_URI)
-console.log(process.env.PORT.MONGODB_URI)
 module.exports = {
   MONGODB_URI,
   PORT,
