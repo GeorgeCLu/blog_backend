b87293dbe6b23fd4e8eb9a4d05447caa572aeaf9
George Lu <georgelu@pop-os.localdomain>
2020-07-11 17:02:27 +1200

    logging

--- utils/config.js
+++ utils/config.js
@@ -6,13 +6,13 @@ const PORT = process.env.PORT || 3001;
 // const PORT = 3001
 let { MONGODB_URI } = process.env;
 // used to be in and used by note.js
-console.log("port is")
-console.log(PORT)
-console.log("mongodb uri is")
-console.log("MONGODB_URI")
 if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI;
 }
+console.log("port is")
+console.log(PORT)
+console.log("mongodb uri is")
+console.log(MONGODB_URI)
 module.exports = {
   MONGODB_URI,
   PORT,
